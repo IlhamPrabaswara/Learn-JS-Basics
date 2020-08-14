742a008f52616043081eb4587d38d8da24e679d1
IlhamPrabaswara <ilhamprabaswara@gmail.com>
2020-08-14 16:56:36 +0700

    Daily updates

--- functions.js
+++ functions.js
@@ -36,3 +36,63 @@ function fun2() {
 }
 fun1();
 fun2();
+
+/*****************************
+ * Local Scope and Functions *
+ *****************************/
+function localScope() {
+    var myVar = 5;
+    console.log(myVar);
+}
+localScope();
+
+/***************************************
+ * Global vs. Local Scope in Functions *
+ ***************************************/
+var outerWear = "T-shirt";
+
+function myOutfit() {
+    var outerWear = "Sweater";
+
+    return outerWear;
+}
+
+console.log(myOutfit());
+console.log(outerWear);
+
+/**********************************************
+ * Return a Value from a Function with Return *
+ **********************************************/
+function minusSeven(num) {
+    return num - 7;
+}
+
+console.log(minusSeven(10));
+
+/**********************************************************
+ * Understanding Undefined Value Returned from a Function *
+ **********************************************************/
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+
+/************************************
+ * Assignment with a Returned Value *
+ ************************************/
+var changed = 0;
+function change(num) {
+    return (num + 5) /3;
+}
+changed = change(10);
+console.log(changed);
+
+/*****************
+ * Stand in Line *
+ *****************/
+function nextInLine(arr, item) {
+    return item;
+}
+
+var testArr = [1, 2, 3, 4, 5];
+

4be08ecb65f584f20564c93e2f52e8624de2435d
IlhamPrabaswara <ilhamprabaswara@gmail.com>
2020-08-26 23:24:40 +0700

    Daily updates

--- functions.js
+++ functions.js
@@ -236,3 +236,106 @@ console.log(testLessThanOrEqual(10));
 /*********************************************
  * Comparisons with the Logical And Operator *
  *********************************************/
+function testLogicalAnd(val) {
+    if (val <= 50 && val >= 25) {
+        return "Yes";
+    }
+    return "No";
+}
+
+console.log(testLogicalAnd(10));
+
+/********************************************
+ * Comparisons with the Logical Or Operator *
+ ********************************************/
+function testLogicalOr(val) {
+    if (val < 10 || val > 20) {
+        return "Outside";
+    }
+    return "Inside";
+}
+console.log(testLogicalOr(15));
+
+/*******************
+ * Else Statements *
+ *******************/
+function testElse(val) {
+    var result = "";
+    if (val > 5) {
+        result = "Bigger than 5";
+    } else {
+        result = "5 or Smaller";
+    }
+    return result;
+}
+console.log(testElse(5));
+
+/**********************
+ * Else If Statements *
+ **********************/
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    } else if (val < 5) {
+        return "Smaller than 5";
+    } else {
+        return "Between 5 and 10";
+    }
+}
+console.log(testElseIf(7));
+
+/***************************************
+ * Logical Order in If Else Statements *
+ ***************************************/
+function orderMyLogic(val) {
+    if (val < 5) {
+        return "Less than 5";
+    } else if (val < 10) {
+        return "Less than 10"
+    } else {
+        return "Greater than or equal to 10"
+    }
+}
+console.log(orderMyLogic(3));
+
+/*******************************
+ * Chaining If Else Statements *
+ *******************************/
+function testSize(num) {
+    if (num < 5) {
+        return "Tiny";
+    } else if (num < 10) {
+        return "Small";
+    } else if (num < 15) {
+        return "Medium";
+    } else if (num < 20) {
+        return "Large";
+    } else {
+        return "Huge";
+    }
+}
+console.log(testSize(7));
+
+/*************
+ * Golf Code *
+ *************/
+var names = ["Hole-in-one", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"]
+function golfScore(par, strokes) {
+    if (strokes == 1) {
+        return names[0];
+    } else if  (strokes <= par - 2) {
+        return names[1];
+    } else if  (strokes == par - 1) {
+        return names[2];
+    } else if  (strokes == par) {
+        return names[3];
+    } else if  (strokes == par + 1) {
+        return names[4];
+    } else if  (strokes == par + 2) {
+        return names[5];
+    } else if  (strokes >= par + 3) {
+        return names[6];
+    }
+}
+
+console.log(golfScore(5, 4));
